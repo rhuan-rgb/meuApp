dfbf75fdf013bf7bbb9d84f40c11925e76d72793
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-03-24 13:36:33 -0300

    adição do olho da senha que faz ela aparecer ao ser clicado

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,71 +1,108 @@
-import React, {useState} from "react";
-import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Button } from "react-native";
-import api from '../axios/axios'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Button,
+} from "react-native";
+import api from "../axios/axios";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Login({navigation}){
-    const [user, setUser] = useState({
-        email:'',
-        password:''
-    })
+export default function Login({ navigation }) {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
 
-    async function handleLogin() {
-        await api.postLogin(user).then(
-           (response)=>{
-                console.log(response.data.message)
-                Alert.alert(response.data.message)
-                navigation.navigate("HomeScreen")
-           },(error)=>{
-                console.log( error)
-                Alert.alert('Erro', error.response.data.error)
-           }
-           
-        )
-    }
+  async function handleLogin() {
+    await api.postLogin(user).then(
+      (response) => {
+        console.log(response.data.message);
+        Alert.alert(response.data.message);
+        navigation.navigate("HomeScreen");
+      },
+      (error) => {
+        console.log(error);
+        Alert.alert("Erro", error.response.data.error);
+      }
+    );
+  }
 
-
-
-    return(
-        <View style={styles.container}>
-        <Text style={styles.title}>Faça Login</Text>
-        <TextInput style={styles.input}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Faça Login</Text>
+      <TextInput
+        style={styles.input}
         placeholder="Email"
         value={user.email}
-        onChangeText={(value)=> {setUser({...user,email: value})} }/>
-
-        <TextInput style={styles.input}
-        placeholder="Senha"
-        value={user.password}
-        onChangeText={(value)=> {setUser({...user,password: value})}}/>
-
-        <TouchableOpacity onPress={handleLogin} style={styles.button}>
-            <Text>Entrar</Text>
+        onChangeText={(value) => {
+          setUser({ ...user, email: value });
+        }}
+      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Senha"
+          value={user.password}
+          secureTextEntry={user.showPassword}
+          onChangeText={(value) => {
+            setUser({ ...user, password: value });
+          }}
+        />
+        <TouchableOpacity onPress={()=>setUser({...user, showPassword:!user.showPassword})}>
+            <Ionicons name={user.showPassword ? 'eye-off' : 'eye'} size={24} color="gray"/>
         </TouchableOpacity>
-        <Button title='Cadastro' onPress={()=> navigation.navigate("Cadastro")}></Button>
-        </View>
-    )
+        
+      </View>
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+      <Button
+        title="Cadastro"
+        onPress={() => navigation.navigate("Cadastro")}
+      ></Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    title:{
-        fontSize:28,
-        fontWeight:'bold'
-    },
-    input:{
-        borderColor:'gray',
-        width:'100%',
-        height:40,
-        borderBottomWidth:1,
-        marginBottom:20,
-        paddingHorizontal:10
-    },
-    button:{
-        backgroundColor:'green',
-        padding:10,
-        boderRadius:5
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  input: {
+    borderColor: "gray",
+    width: "100%",
+    height: 40,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 10,
+    boderRadius: 5,
+  },
+  passwordContainer:{
+    flexDirection: 'row',
+    alignItems:"center",
+    width:"100%",
+    paddingRight:10,
+    borderBottomWidth: 1
+  },
+  passwordInput:{
+    flex:1,
+    height:40,
+
+  }
+});
