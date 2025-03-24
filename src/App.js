232d99244d3583c9bcaf02a486df31368d41b11a
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-03-24 15:07:14 -0300

    ultimo commit 24-03

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="Login" component={()=>(
           <Layout>
             <Login/>
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,110 +1,107 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  StyleSheet,
-  Button,
-} from "react-native";
-import api from "../axios/axios";
-import { Ionicons } from "@expo/vector-icons";
+import React, {useState} from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Button } from "react-native";
+import api from "../axios/axios"
+import {Ionicons} from "@expo/vector-icons"
+import {useNavigation} from "@react-navigation/native"
 
-export default function Login({ navigation }) {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-    showPassword: false,
-  });
 
-  async function handleLogin() {
-    await api.postLogin(user).then(
-      (response) => {
-        console.log(response.data.message);
-        Alert.alert(response.data.message);
-        navigation.navigate("HomeScreen");
-      },
-      (error) => {
-        console.log(error);
-        Alert.alert("Erro", error.response.data.error);
-      }
-    );
-  }
+export default function Login() {
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Faça Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={user.email}
-        onChangeText={(value) => {
-          setUser({ ...user, email: value });
-        }}
-      />
-      
-      <View style={styles.passwordContainer}>
-        <TextInput
-          style={styles.passwordInput}
-          placeholder="Senha"
-          value={user.password}
-          secureTextEntry={user.showPassword}
-          onChangeText={(value) => {
-            setUser({ ...user, password: value });
-          }}
-        />
-        <TouchableOpacity onPress={()=>setUser({...user, showPassword:!user.showPassword})}>
-            <Ionicons name={user.showPassword ? 'eye-off' : 'eye'} size={24} color="gray"/>
-        </TouchableOpacity>
-        
-      </View>
+    const navigation = useNavigation()
 
-      <TouchableOpacity onPress={handleLogin} style={styles.button}>
-        <Text>Entrar</Text>
-      </TouchableOpacity>
-      <Button
-        title="Cadastro"
-        onPress={() => navigation.navigate("Cadastro")}
-      ></Button>
-    </View>
-  );
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+        showPassword: false
+    })
+
+    async function handleLogin() {
+        await api.postLogin(user)
+        .then(
+            (response) => {
+                console.log(response.data.message)
+                navigation.navigate("Home")
+            }, (error) => {
+                console.log(error)
+                Alert.alert(error.response.data.error)
+            }
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Faça Login</Text>
+
+            <View style={styles.emailContainer}>
+                <TextInput
+                    placeholder="E-mail"
+                    value={user.email}
+                    onChangeText={(value) => {setUser({...user, email: value})}}
+                    style={styles.emailInput}
+                />
+            </View>
+            
+
+            <View style={styles.passwordContainer}>
+                <TextInput
+                    placeholder="Senha"
+                    value={user.password}
+                    onChangeText={(value) => {setUser({...user, password: value})}}
+                    style={styles.passwordInput}
+                    secureTextEntry={user.showPassword}
+                />
+
+                <TouchableOpacity onPress={() => setUser({...user, showPassword: !user.showPassword })}>
+                    <Ionicons name={user.showPassword? "eye-off" : "eye"} size={34} color="gray"/>
+                </TouchableOpacity>
+            </View>
+            
+
+            <TouchableOpacity onPress = {handleLogin} style={styles.button}>
+                <Text>Login</Text>
+            </TouchableOpacity>
+            
+            <Button title="Cadastro" onPress={()=> navigation.navigate("Cadastro")}/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-  },
-  input: {
-    borderColor: "gray",
-    width: "90%",
-    height: 40,
-    borderBottomWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    fontSize:20,
-  },
-  button: {
-    backgroundColor: "green",
-    padding: 10,
-    boderRadius: 5,
-  },
-  passwordContainer:{
-    flexDirection: 'row',
-    alignItems:"center",
-    width:"100%",
-    paddingRight:10,
-    borderBottomWidth: 1
-  },
-  passwordInput:{
-    flex:1,
-    height:40,
-
-  }
-});
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: 'center'
+  
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: '#006400',
+        padding: 10,
+        borderRadius: 5,
+    },
+    passwordContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderBottomWidth: 1,
+        width: "100%",
+        paddingRight: 10,
+    },
+    emailContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderBottomWidth: 1,
+        width: "100%",
+        paddingRight: 10,
+    },
+    passwordInput: {
+        flex: 1,
+        height: 40,
+    },
+    emailInput: {
+        flex: 1,
+        height: 40,
+    }
+})
